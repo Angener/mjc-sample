49cb55f1fe5c7b9fda38b48c057e2921255eba2d
Artyom Olshevskiy <artyom.olshevskiy@gmail.com>
2020-12-02 12:55:05 +0200

    Initial commit

---
+++ src/main/java/mjcsample/SampleConfiguration.java
@@ -0,0 +1,9 @@
+package mjcsample;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class SampleConfiguration {
+}
